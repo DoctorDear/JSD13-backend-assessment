62a7d4b384b8b6f676aa064a16f5c840f48b104b
Chirasak Tapphae <dearchirasak@gmail.com>
2026-09-11 19:34:23 +0700

    feat: add AdminView component for product management and searching

--- client/src/components/AdminView.jsx
+++ client/src/components/AdminView.jsx
@@ -36,63 +36,69 @@ const AdminView = ({
 
   return (
     <div className="flex flex-col gap-8 my-6">
-      <div className="flex flex-col items-start w-fit mx-auto gap-3">
-        <h2 className="font-bold text-lg text-black">Add New Product</h2>
-        <form onSubmit={handleSubmit} className="flex justify-center gap-4">
-          <input
-            className="input validator"
-            type="text"
-            placeholder="Name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          <input
-            type="number"
-            placeholder="Price"
-            min="0"
-            value={price}
-            onChange={(e) => {
-              const val = e.target.value;
-              if (val === "" || Number(val) >= 0) {
-                setPrice(val);
-              }
-            }}
-            className="input validator"
-          />
-          <input
-            type="number"
-            placeholder="Quantity"
-            min="0"
-            value={quantity}
-            onChange={(e) => {
-              const val = e.target.value;
-              if (val === "" || Number(val) >= 0) {
-                setQuantity(val);
-              }
-            }}
-            className="input validator"
-          />
-          <button type="submit" className="btn bg-blue-500 text-white ">
-            Create
-          </button>
-        </form>
-      </div>
-      <div className="flex flex-col items-start w-fit mx-auto gap-3">
-        <h2 className="font-bold text-lg text-black">Search Product</h2>
-        <form
-          onSubmit={handleSeach}
-          className="flex justify-center items-center gap-3"
-        >
-          <input
-            type="text"
-            placeholder="Search products by name..."
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-            className="input validator"
-          />
-          <button className="btn bg-blue-600 text-white">Search</button>
-          {searchTerm && <button className="btn">clear</button>}
-        </form>
+      <div className="flex flex-col items-start w-fit   gap-6">
+        <div className="flex flex-col items-start  gap-3">
+          <h2 className="font-bold text-lg text-black">Add New Product</h2>
+          <form onSubmit={handleSubmit} className="flex justify-center gap-4">
+            <input
+              className="input validator"
+              type="text"
+              placeholder="Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <input
+              type="number"
+              placeholder="Price"
+              min="0"
+              value={price}
+              onChange={(e) => {
+                const val = e.target.value;
+                if (val === "" || Number(val) >= 0) {
+                  setPrice(val);
+                }
+              }}
+              className="input validator"
+            />
+            <input
+              type="number"
+              placeholder="Quantity"
+              min="0"
+              value={quantity}
+              onChange={(e) => {
+                const val = e.target.value;
+                if (val === "" || Number(val) >= 0) {
+                  setQuantity(val);
+                }
+              }}
+              className="input validator"
+            />
+            <button type="submit" className="btn bg-blue-500 text-white ">
+              Create
+            </button>
+          </form>
+        </div>
+        <div className="flex flex-col items-start  gap-3">
+          <h2 className="font-bold text-lg text-black">Search Product</h2>
+          <form
+            onSubmit={handleSeach}
+            className="flex justify-center items-center gap-3"
+          >
+            <input
+              type="text"
+              placeholder="Search products by name..."
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              className="input validator"
+            />
+            <button className="btn bg-blue-600 text-white">Search</button>
+            {searchTerm && (
+              <button type="button" onClick={handleReset} className="btn">
+                clear
+              </button>
+            )}
+          </form>
+        </div>
       </div>
       <Table
         products={products}
